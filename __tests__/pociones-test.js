972c2ec3596d45919664f78ea0194b7992cebe1e
carlosvalarezo <carlos.valarezo@gmail.com>
2020-09-09 16:02:05 -0500

    [@carlosvalarezo] tdd con 6, 7, 10 pociones

--- __tests__/pociones-test.js
+++ __tests__/pociones-test.js
@@ -2,10 +2,30 @@ const pociones = require("../src/Pociones.js")
 
 test('retorna 3% dada 1 poción', () => {
   dano = pociones.CalcularDano(1);
-  expect(3).toBe(dano)
+  expect(dano).toBe(3)
 })
 
 test('retorna 6% dada 2 pociones', () => {
   dano = pociones.CalcularDano(2);
-  expect(6).toBe(dano)
+  expect(dano).toBe(6)
+})
+
+test('retorna 10% dada 5 pociones', () => {
+  dano = pociones.CalcularDano(5);
+  expect(dano).toBe(25)
+})
+
+test('retorna 28% dada 6 pociones', () => {
+  dano = pociones.CalcularDano(6);
+  expect(dano).toBe(28)
+})
+
+test('retorna 31% dada 7 pociones', () => {
+  dano = pociones.CalcularDano(7);
+  expect(dano).toBe(31)
+})
+
+test('retorna 50% dada 10 pociones', () => {
+  dano = pociones.CalcularDano(10);
+  expect(dano).toBe(50)
 })
--- src/Pociones.js
+++ src/Pociones.js
@@ -2,9 +2,13 @@ const Pociones = {
 
   CalcularDano: pociones => {
     let dano = 0;
+    let pocionesRestantes = 0;
 
     while (pociones > 0){
-
+      if (pociones > 5){
+        pocionesRestantes = Math.abs(pociones - 5);
+        pociones = 5;
+      }
       switch(pociones){
         case 1:
           dano += 3;
@@ -13,7 +17,15 @@ const Pociones = {
         case 2: dano += 3; pociones -= 1; break;
         case 3: dano += 10; pociones -= 3; break;
         case 4: dano += 20; pociones -= 4; break;
-        case 5: dano += 25; pociones -= 5; break;
+        case 5:
+            dano += 25;
+            pociones -= 5;
+            if (pocionesRestantes > 0) {
+              pociones = pocionesRestantes;
+              pocionesRestantes -= pociones;
+            }
+            console.log("pociones 5", pociones);
+            break;
         default: break;
       }
 
